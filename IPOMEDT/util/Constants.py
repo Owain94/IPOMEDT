4d4a5c5eb082d5bb5261bbf90cdf24db89c61133
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-01-30 14:01:23 +0100

    Add a class to declare constants

---
+++ IPOMEDT/util/Constants.py
@@ -0,0 +1,45 @@
+class Constants(object):
+    """
+    Klasse om constante variabele aan te kunnen maken
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """
+        Code die wordt uitgevoerd bij het instantiëren van de klasse
+        :param args: args
+        :param kwargs: kwargs
+        """
+        self._d = dict(*args, **kwargs)
+
+    def __iter__(self) -> iter:
+        """
+        Implementeer iteraties voor de dictionary met attributen
+        :return: iteratie van de dictionary als iter
+        """
+        return iter(self._d)
+
+    def __len__(self) -> int:
+        """
+        Geeft het aantal attributen terug
+        :return: het aantal attributen als int
+        """
+        return len(self._d)
+
+    def __getattr__(self, name: str) -> any:
+        """
+        Getter voor een attribuut
+        :param name: Attribuut naam als string
+        :return: Attribuut waarde als *
+        """
+        return self._d[name]
+
+    def __setattr__(self, name: str, value: any) -> None:
+        """
+        Attribuut setter
+        :param name: Attribuut naam als string
+        :param value: Attribuut value als *
+        """
+        if name[0] == '_':
+            super(Constants, self).__setattr__(name, value)
+        else:
+            raise ValueError("setattr while locked", self)
